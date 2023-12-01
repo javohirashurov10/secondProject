28c5e7ef618c347286ad7a6bfb887bb0a4e1a37c
javohirashurov <shehrozashurov63@gmail.com>
2023-12-01 19:32:05 +0300

    first commit

---
+++ 14-dars.py
@@ -0,0 +1,18 @@
+talaba_0 = {
+    'ism':'alijon',
+    'familiya':'shamshiyev',
+    'yosh':22,
+    'fakultet':'matematika',
+    'kurs':4
+    }
+print(talaba_0.items())
+for kalit, qiymat in talaba_0.items():
+    print(f"Kalit: {kalit}")
+    print(f"Qiymat: {qiymat}/n")
+telefonlar = {
+    'aliy':'iphone x',
+    'vali':'galaxiy s9',
+    'olim':'mi 10 pro',
+    'orif':'nokiya 3310',
+    }
+      
